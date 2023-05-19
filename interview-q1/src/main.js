61dd23a1eb10e18c03d5f0651f8950f2b4bfd871
Vikim_294 <1920829170@qq.com>
2023-05-19 10:48:43 +0800

    element-ui 的安装 和 按需引入的配置

--- interview-q1/src/main.js
+++ interview-q1/src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import { Button, Select } from "element-ui";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Button);
+Vue.use(Select);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
